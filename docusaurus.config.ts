f621f742ca867f3b0850cfcc8724fa42a6b57c36
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-04-12 19:05:36 +0400

    Переименовал заметки в посты

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
         { to: "/docs/channels", label: "Каналы", position: "left" },
         { to: "/docs/publications", label: "Публикации", position: "left" },
         { to: "/docs/projects", label: "Проекты", position: "left" },
-        { to: "/docs/category/заметки", label: "Заметки", position: "left" },
+        { to: "/docs/category/посты", label: "Посты", position: "left" },
         { to: "/docs/contacts", label: "Контакты", position: "left" },
       ],
     },
