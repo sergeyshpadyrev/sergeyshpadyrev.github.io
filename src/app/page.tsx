c16f32e916e43357833a2a0c7fe61bb2fa37bf0b
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-01-11 17:37:12 +0400

    Add next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        fontFamily: "system-ui, sans-serif",
+      }}
+    >
+      <h1 style={{ fontSize: "3rem" }}>Сергей Шпадырев</h1>
+    </main>
+  );
+}
